baf8842d26e03c3163909a433c201cc2bd7e386f
Hao Nguyen <ngvhao172@gmail.com>
2025-01-10 16:27:16 +0700

    fix(postcss): fix

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,10 @@
 // it will create an unexpected error: Invalid PostCSS Plugin found: [0]
 
 /** @type {import('postcss-load-config').Config} */
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
-  ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+  },
 };
