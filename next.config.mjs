e9bb7f8aeccaedd1471d042d75af39eb9e8f3b6b
Hao Nguyen <ngvhao172@gmail.com>
2025-01-12 00:05:59 +0700

    fix(all): fix all

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,55 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import withNextIntl from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/app/shared/lib/i18n.ts");
+const withNextIntlConfig = withNextIntl('./src/shared/libs/i18n.ts');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-export default withNextIntl(nextConfig);
+/* @type {import('next').NextConfig} */
+export default bundleAnalyzer(
+  withNextIntlConfig({
+    compiler: {
+      styledComponents: true,
+    },
+    poweredByHeader: false,
+    reactStrictMode: true,
+    experimental: {
+      serverComponentsExternalPackages: ['@electric-sql/pglite'],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 's3-ap-northeast-1.amazonaws.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'ecbo-production.s3-ap-northeast-1.amazonaws.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cloak.ecbo.io',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.pinimg.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'ecbo-cloak-production.s3.ap-northeast-1.amazonaws.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 's3-alpha-sig.figma.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn-poc-luggage-s3.s3.ap-northeast-1.amazonaws.com',
+        },
+      ],
+    },
+  }),
+);
+
