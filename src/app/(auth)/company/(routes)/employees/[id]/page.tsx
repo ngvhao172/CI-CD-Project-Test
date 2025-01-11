29e7b9d5cebc1e2210db79f13e576c1e8223ab83
Hao Nguyen <ngvhao172@gmail.com>
2025-01-12 00:43:42 +0700

    feat(all): add generateStaticParams

--- src/app/(auth)/company/(routes)/employees/[id]/page.tsx
+++ src/app/(auth)/company/(routes)/employees/[id]/page.tsx
@@ -1,5 +1,13 @@
 import { EmployeeAttendance } from './components/EmployeeAttendance';
 
+export async function generateStaticParams() {
+  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return ids.map((id) => ({
+    id: id.toString(),
+  }));
+}
+
 export default function EmployeeAttendancePage() {
   return (
     <div className="space-y-6">
