94b4ffd807d1e9e6f65be21d41236f540921287d
Hao Nguyen <ngvhao172@gmail.com>
2025-01-12 00:13:25 +0700

    fix(all): add bundle analyze build

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
 import withNextIntl from 'next-intl/plugin';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const withNextIntlConfig = withNextIntl('./src/shared/libs/i18n.ts');
+const withNextIntlConfig = withNextIntl('./src/app/shared/lib/i18n.ts');
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
 /* @type {import('next').NextConfig} */
-export default bundleAnalyzer(
-  withNextIntlConfig({
-    compiler: {
-      styledComponents: true,
-    },
-    poweredByHeader: false,
-    reactStrictMode: true,
-    experimental: {
-      serverComponentsExternalPackages: ['@electric-sql/pglite'],
-    },
-  }),
-);
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@electric-sql/pglite'],
+  },
+};
+
+export default bundleAnalyzer(withNextIntlConfig(nextConfig));
 
